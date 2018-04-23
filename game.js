63c8f3f4dd36869ec233f7926df12ce9f58306bb
Andrey <andrey.dremuchev@gmail.com>
2018-04-23 20:51:14 +0300

    '23.04.2018'

--- game.js
+++ game.js
@@ -2,18 +2,15 @@
 
 class Vector {
     constructor(x = 0, y = 0) {
-        this.x = x,
+        this.x = x;
         this.y = y;
     }
 
     plus(vector) {
         if (!(vector instanceof Vector)) {
             throw new Error('Можно прибавлять к вектору только вектор типа Vector!');
-          // else лишний, если будет выброшено исключение функция прекратит выполнение
-        } else {
-            return new Vector(this.x + vector.x, this.y + vector.y);
-
         }
+        return new Vector(this.x + vector.x, this.y + vector.y);
     }
 
     times(multiplier) {
@@ -26,13 +23,10 @@ class Actor {
     constructor(pos = new Vector(), size = new Vector(1, 1), speed = new Vector()) {
         if (!(pos instanceof Vector && size instanceof Vector && speed instanceof Vector)) {
             throw new Error('Неверные аргументы!');
-          // else лишний, если будет выброшено исключение функция прекратит выполнение
-        } else {
-            // почему запятые? не нужно так писать
-            this.pos = pos,
-            this.size = size,
-            this.speed = speed
         }
+        this.pos = pos;
+        this.size = size;
+        this.speed = speed;
     }
 
     act() {}
@@ -64,45 +58,32 @@ class Actor {
         if (this === movingObject) {
             return false;
         }
-        // отрицание можно внести в скобки заменив || на && и операторы на противоположные
-        // <= на >, >= на <
-        return (!(movingObject.bottom <= this.top || movingObject.top >= this.bottom || movingObject.right <= this.left || movingObject.left >= this.right));
+        return (movingObject.bottom > this.top && movingObject.top < this.bottom && movingObject.right > this.left && movingObject.left < this.right);
     }
 }
 
 
 class Level {
     constructor(grid = [], actors = []) {
-        // здесь лучше создать копии массивов
-        this.grid = grid;
-        this.actors = actors;
+        this.grid = grid.slice();
+        this.actors = actors.slice();
         this.player = actors.find((el) => el.type === 'player');
         this.status = null;
         this.finishDelay = 1;
-        // эта проверка всё ещё не имеет смысле, т.к. если аргументы не будут переданы, то работать ничего не будет
-        if (arguments.length !== 0) {
-            const nextArray = grid.map((el) => el.length);
-            this.width =  Math.max(...nextArray);
-            this.height = grid.length;
-        } else {
-            this.width = 0;
-            this.height = 0;
-        }
-
+        const nextArray = grid.map((el) => el.length);
+        this.width =  Math.max(0, ...nextArray);
+        this.height = grid.length;
     }
 
     isFinished() {
-        // скобки можно убрать
-        return (status !== null && this.finishDelay < 0);
+        return status !== null && this.finishDelay < 0;
     }
 
     actorAt(actor) {
         if (!(actor instanceof Actor) || actor === null) {
             throw new Error('Неверный аргумент!');
-            // else лишний
-        } else {
-            return (this.actors.find((el) => el.isIntersect(actor)));
         }
+        return (this.actors.find((el) => el.isIntersect(actor)));
     }
 
     obstacleAt(pos, size) {
@@ -111,43 +92,40 @@ class Level {
         }
         if(pos.x < 0  || pos.x > this.width - size.x || pos.y < 0) {
             return 'wall';
-            // else лишний
-        } else if(pos.y > this.height - size.y) {
+        }
+        if(pos.y > this.height - size.y) {
             return 'lava';
         }
-
-        // границы лучше записать в переменные,
-        // чтобы было понятнее и чтобы раждый раз не складывать и не округлять
-        for(let i = Math.floor(pos.y); i < Math.ceil(pos.y + size.y); i++) {
-            for(let j = Math.floor(pos.x); j < Math.ceil(pos.x + size.x); j++) {
-                // this.grid[i][j] лучше записать в переменную, чтобы не писать 2 раза
-                if(this.grid[i][j]) {
-                    return this.grid[i][j];
+        const top = Math.floor(pos.y),
+        bottom = Math.ceil(pos.y + size.y),
+        left = Math.floor(pos.x),
+        right = Math.ceil(pos.x + size.x);
+        for(let i = top; i < bottom; i++) {
+            for(let j = left; j < right; j++) {
+                const foundObstacle = this.grid[i][j];
+                if(foundObstacle) {
+                    return foundObstacle;
                 }
             }
         }
     }
 
     removeActor(actor) {
-        // неудачное название переменной - непонятно, что в ней index,
-        // правильнее было бы foundIndex
-        const finded = this.actors.indexOf(actor);
-        if (finded >= 0) {
-            this.actors.splice(finded, 1);
+        const foundIndex = this.actors.indexOf(actor);
+        if (foundIndex >= 0) {
+            this.actors.splice(foundIndex, 1);
         }
     }
 
     noMoreActors(actorType) {
-        // скобки можно убрать
-        return (!this.actors.some((el) => el.type === actorType));
+        return !this.actors.some((el) => el.type === actorType);
 
     }
 
     playerTouched(object, actor) {
         if(object === 'lava' || object === 'fireball') {
             this.status = 'lost';
-            // если убрать проверку actor ничего не изменится
-        } else if (object === 'coin' && actor !== undefined) {
+        } else if (object === 'coin') {
             this.removeActor(actor);
             if(this.noMoreActors(object)) {
                 this.status = 'won';
@@ -158,8 +136,7 @@ class Level {
 
 class LevelParser {
     constructor(actorDict = {}) {
-        // тут можно использовать оператор spread
-        this.actorDict = Object.assign({}, actorDict);
+        this.actorDict = {...actorDict};
     }
 
     actorFromSymbol(sym) {
@@ -176,30 +153,33 @@ class LevelParser {
     }
 
     createGrid(array) {
-        // тут можно написать понятнее использововав метод map 2 раза
-        const mapLevel = array.map((el) => el.split(''));
-        mapLevel.forEach((row) => {
-            row.map((cell, numIndex) => {
-            row[numIndex] = this.obstacleFromSymbol(cell);
-            })
-        })
-        return mapLevel;
+        const mapLevel = array.map( (el) => el.split('') );
+        return mapLevel.map( (row) => row.map( (cell) => this.obstacleFromSymbol(cell) ) );
     }
 
     createActors(array) {
         const actorMap =[];
         const tempArray = array.map((el) => el.split(''));
-        tempArray.forEach((row, rowIndex) => {
-            row.forEach((cell, numIndex ) => {
-                // значение присваивается переменной 1 раз, лучше использовать const
-                let isConstructor = this.actorDict[cell];
-                // зачем такие сложные конструкции, как я писал в прошлый раз,
-                // почему бы не создать объект 1 раз, проверить его тип, и,
-                // если он подходит - добавить в массив
-                if(typeof(isConstructor) === 'function' && Object.create(isConstructor.prototype) instanceof Actor) {
-                    actorMap.push(new isConstructor(new Vector(numIndex, rowIndex)) );
+        console.log(tempArray);
+        tempArray.forEach( (row, rowIndex) => { row.forEach( (cell, numIndex) => {
+            const isConstructor = this.actorDict[cell];
+
+            /*
+            если я создаю объект на основе константы isConstructor,
+            я получаю ошибку, undefined - не является конструктором,
+            поэтому, считаю, что целесообразнее проверить константу
+            на принадлежность к конструкторам, а уже потом создавать
+            объект с последующей проверкой на наследование...
+            считаю, что он боле менее краток и понятен.
+            */
+
+            if(typeof (isConstructor) === 'function') {
+                const actor = new isConstructor(new Vector(numIndex, rowIndex));
+                if(actor instanceof Actor) {
+                    actorMap.push(actor);
                 }
-            })
+            }
+        })
         })
         return actorMap;
     }
@@ -209,6 +189,7 @@ class LevelParser {
     }
 
 }
+
 
 class Fireball extends Actor {
     constructor(pos, speed) {
@@ -239,24 +220,21 @@ class Fireball extends Actor {
 }
 
 class HorizontalFireball extends Fireball {
-    // должен принимать 1 аргумент
-    constructor(pos, speed) {
+    constructor(pos) {
         super(pos, new Vector(2, 0));
     }
 
 }
 
 class VerticalFireball extends Fireball {
-    // должен принимать 1 аргумент
-    constructor(pos, speed) {
+    constructor(pos) {
         super(pos, new Vector(0, 2));
     }
 
 }
 
 class FireRain extends Fireball {
-    // лучше не опускать аргументы конструктора Vector
-    constructor(pos = new Vector()) {
+    constructor(pos) {
         super(pos, new Vector(0, 3));
         this.basePos = pos;
 
@@ -268,12 +246,15 @@ class FireRain extends Fireball {
 }
 
 class Coin extends Actor {
-    constructor(pos) {
-        super(pos, new Vector(0.6, 0.6));
-        // pos лучше задавать через конструктор родительского класса
-        this.pos = this.pos.plus(new Vector(0.2, 0.1));
-        // Math.random принимает 1 аргумент
-        this.spring = Math.random(0, 2 * Math.PI);
+
+    /*
+    добавил значение по умолчанию дабы исключить ошибку при
+    создании монетки без аргументов new Coin()
+     */
+
+    constructor(pos = new Vector()) {
+        super(pos.plus(new Vector(0.2, 0.1)), new Vector(0.6, 0.6));
+        this.spring = Math.random() * 2 * Math.PI;
         this.springSpeed = 8;
         this.springDist = 0.07;
         this.basePos = this.pos;
@@ -304,10 +285,14 @@ class Coin extends Actor {
 }
 
 class Player extends Actor {
-    constructor(pos) {
-        super(pos, new Vector(0.8, 1.5));
-        // pos лучше задавать через конструктор родительского класса
-        this.pos = new Vector(this.pos.x, this.pos.y - 0.5);
+
+    /*
+    добавил значение по умолчанию дабы исключить ошибку при
+    создании монетки без аргументов new Player()
+     */
+
+    constructor(pos = new Vector()) {
+        super(new Vector(pos.x, pos.y - 0.5), new Vector(0.8, 1.5));
     }
 
     get type() {
